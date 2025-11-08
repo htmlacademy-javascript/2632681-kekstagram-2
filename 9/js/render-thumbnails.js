3d7323d9f9722b6887a16d418549f2f074492621
Keks <keks@htmlacademy.ru>
2025-11-08 05:18:11 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/render-thumbnails.js
@@ -0,0 +1,33 @@
+import {renderFullSizePicture} from './render-full-size-image.js';
+
+const picturesList = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const renderThumbnails = (pictures) => {
+
+  const picturesFragment = document.createDocumentFragment();
+
+  pictures.forEach((picture) => {
+    const {url, description, likes, comments} = picture;
+    const pictureElement = picturesTemplate.cloneNode(true);
+    const image = pictureElement.querySelector('.picture__img');
+
+    image.src = url;
+    image.alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+    pictureElement.addEventListener('click', () => {
+      renderFullSizePicture(picture);
+    });
+
+
+    picturesFragment.appendChild(pictureElement);
+  });
+
+  picturesList.appendChild(picturesFragment);
+};
+
+export {renderThumbnails};
